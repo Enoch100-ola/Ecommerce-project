16eb47b1fbe06b40f11204f53f3d66827e47d472
Enoch <enochola96@gmail.com>
2021-07-09 02:35:05 +0100

    admin working fine

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -19,6 +19,9 @@ from django.urls import path
 from online_app import views
 from ecommerce_app import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name='home'),
@@ -26,3 +29,7 @@ urlpatterns = [
     path('online_app/', include('online_app.urls')),
     path('ecommerce/', include('ecommerce_app.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
