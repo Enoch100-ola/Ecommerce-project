5c48d1cc5f06b67f38b616433d6167192f6b1308
Enoch <enochola96@gmail.com>
2021-08-24 10:51:18 +0100

    error resolved

--- ecommerce_app/models.py
+++ ecommerce_app/models.py
@@ -1,10 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-<<<<<<< HEAD
-=======
 from django.shortcuts import reverse
-
->>>>>>> master
 # from django.db.models.base import Model
 from django.shortcuts import reverse
 
